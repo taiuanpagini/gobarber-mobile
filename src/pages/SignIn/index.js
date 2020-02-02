4ddfe662b8e88b200bce926037c94ea3330c1e2f
Wanderlei Santos <wanderleisantosdias@gmail.com>
2020-02-01 22:09:59 -0300

    Página SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,47 @@
 import React from 'react';
+import {Image} from 'react-native';
+
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
-// import { Container } from './styles';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+import logo from '~/assets/logo.png';
 
 export default function SignIn() {
   return (
     <Background>
-      <Input
-        icon="call"
-        placeholder="Digite seu nome"
-        style={{marginTop: 30}}
-      />
+      <Container>
+        <Image source={logo} />
 
-      <Button>Entrar</Button>
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText> Criar conta </SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
