fa0bea62ad169c9bdd8e8f8d41980eff6e60ac53
Wanderlei Santos <wanderleisantosdias@gmail.com>
2020-02-01 21:45:46 -0300

    Input & Button

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Input
+        icon="call"
+        placeholder="Digite seu nome"
+        style={{marginTop: 30}}
+      />
+
+      <Button>Entrar</Button>
+    </Background>
+  );
+}
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+export default function SignUp() {
+  return (
+    <Background>
+      <Text>DSADS DEU CERTO </Text>
+    </Background>
+  );
+}
